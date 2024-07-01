1de1d81ed51baaa96f2805e70c01afb42eb5d853
Eduardo-Brito1 <kabisashi2@gmail.com>
2024-07-01 15:24:44 -0300

    função para codificar o texto

--- index.js
+++ index.js
@@ -11,21 +11,53 @@ function botaoClicavel() {
     if (VerificarTexto() === true) {
         alert('não pode enviar letra maiuscula ou acentos')
     } else {
-
         conteudoModal.style.display = 'none'
         conteudoMensagem.style.display = 'none'
+        textoEscrito.value = codificarTexto()
 
         Mensagem.innerHTML = textoEscrito.value
+        textoEscrito.value = '';
 
         mostrarButton()
+
     }
 }
 
-function VerificarTexto(vericacao) {
-    for (let i = 0; i < textoEscrito.value.length; i++) {
-        let caractere = textoEscrito.value[i];
 
-        if (caractere >= "A" && caractere <= "Z" || acentoRegex.test(caractere) === true) {
+function codificarTexto() {
+    let novoTexto = ''
+
+    for (let i = 0; i < textoEscrito.value.length; i++) {
+        let valorDigitado = textoEscrito.value
+        console.log(valorDigitado)
+        if (valorDigitado[i] === 'a') {
+            novoTexto = novoTexto + "ai"
+        } else if (valorDigitado[i] === 'e') {
+            novoTexto = novoTexto + 'enter'
+        } else if (valorDigitado[i] === 'i') {
+            novoTexto = novoTexto + 'imes'
+        } else if (valorDigitado[i] === 'o') {
+            novoTexto = novoTexto + 'ober'
+        } else if (valorDigitado[i] === 'u') {
+            novoTexto = novoTexto + 'ufat'
+        } else {
+            novoTexto = novoTexto + valorDigitado[i]
+        }
+    }
+    console.log(novoTexto)
+
+    return novoTexto
+}
+
+
+
+function VerificarTexto(vericacao) {
+
+
+    for (let i = 0; i < textoEscrito.value.length; i++) {
+        let char = textoEscrito.value[i];
+
+        if (char >= "A" && char <= "Z" || acentoRegex.test(char) === true) {
             vericacao = true
         }
     }
